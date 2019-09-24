3bcbd6e51339b1299b690fd92143575928a2c99a
julianutn <juliancai94@gmail>
2019-09-24 15:01:29 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -8,6 +8,14 @@ var cors        = require('cors');
  
 var app = express();
 app.use(cors());
+app.use((req, res, next) => {
+
+    res.header(Access-Control-Allow-Headers, Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method);
+    res.header(Access-Control-Allow-Methods, GET, POST, OPTIONS, PUT, DELETE);
+    res.header(Allow, GET, POST, OPTIONS, PUT, DELETE);
+    next();
+});
+
  
 // get our request parameters
 app.use(bodyParser.urlencoded({ extended: false }));
