ed520d53ae386a68a11879d849f33f9d8f762720
julianutn <juliancai94@gmail>
2019-10-02 19:12:35 -0300

    Update mascota.model.js

--- src/models/mascota.model.js
+++ src/models/mascota.model.js
@@ -43,12 +43,7 @@ var MascotaSchema = new mongoose.Schema({
     {
         type: Boolean,
         required: false
-    },
-    amo:
-    [{     
-        type: mongoose.Types.ObjectId,
-        ref: "User"
-    }]
+    }
 });
 
 //quizas acá se podria agregar una funcion que agregue el ID object de mongoDB del usuario a la mascota 
