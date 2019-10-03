a31f424e49620d8d1a488eb4aa0f13c7f6c11bd7
julianutn <juliancai94@gmail>
2019-10-03 11:06:54 -0300

    Update mascota-controller.js

--- src/controller/mascota-controller.js
+++ src/controller/mascota-controller.js
@@ -16,7 +16,7 @@ exports.registerMascota = (req, res) => {
         }
         return res.status(201).json( { mascota: (newMascota), 
                                        msj:"Guau! Nuevo Perfil Creado!",
-                                       usuario: userc.controlUser(token)
+                                 
         });
     });
 };
