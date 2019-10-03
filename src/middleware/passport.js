81981a6cbcfd05bc074746f6cab284b39cf649e9
julianutn <juliancai94@gmail>
2019-10-03 18:52:55 -0300

    g

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -20,22 +20,3 @@ module.exports = new JwtStrategy(opts, function (jwt_payload, done) {
         }
     });
 });
-exports.ensureAuthenticated = function(req, res, next) {
-    if(!req.headers.authorization) {
-      return res
-        .status(403)
-        .send({message: "Tu petición no tiene cabecera de autorización"});
-    }
-    
-    var token = req.headers.authorization.split(" ")[1];
-    var payload = jwt.decode(token, config.jwtSecret);
-    
-    if(false) {
-       return res
-           .status(401)
-          .send({message: "El token ha expirado"});
-    }
-    
-    req.user = payload.sub;
-    next();
-  }
