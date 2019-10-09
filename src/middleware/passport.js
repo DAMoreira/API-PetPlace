e9fef96c5ff55c5363f7cb85c119e74bc13aad1e
julianutn <juliancai94@gmail>
2019-10-09 13:55:18 -0300

    Update passport.js

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -3,15 +3,13 @@ var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt  = require('passport-jwt').ExtractJwt;
 var config      = require('../config/config');
  
-var JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
-var opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey= config.jwtSecret
-opts.issuer = 'accounts.examplesoft.com';
-opts.audience = 'yoursite.net';
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.sub}, function(err, user) {
+var opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.jwtSecret
+}
+
+module.exports = new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findById(jwt_payload.id, function (err, user) {
         if (err) {
             return done(err, false);
         }
@@ -19,7 +17,6 @@ passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
             return done(null, user);
         } else {
             return done(null, false);
-            // or you could create a new account
         }
     });
-}));
+});
