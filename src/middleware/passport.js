36f93bf689e8699b7045826051bb718c87a17ebd
julianutn <juliancai94@gmail>
2019-10-09 12:05:52 -0300

    Update passport.js

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -4,7 +4,7 @@ var JwtStrategy = require('passport-jwt').Strategy,
 var config      = require('../config/config');
  
 var opts = {
-    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: config.jwtSecret
 }
 
