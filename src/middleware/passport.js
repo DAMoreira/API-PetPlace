4afe9bb363c309b056d301788360ce5d9698ee1f
julianutn <juliancai94@gmail>
2019-10-09 13:52:13 -0300

    flashando

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -3,13 +3,15 @@ var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt  = require('passport-jwt').ExtractJwt;
 var config      = require('../config/config');
  
-var opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: config.jwtSecret
-}
-
-module.exports = new JwtStrategy(opts, function (jwt_payload, done) {
-    User.findById(jwt_payload.id, function (err, user) {
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey= config.jwtSecret
+opts.issuer = 'accounts.examplesoft.com';
+opts.audience = 'yoursite.net';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
         if (err) {
             return done(err, false);
         }
@@ -17,6 +19,7 @@ module.exports = new JwtStrategy(opts, function (jwt_payload, done) {
             return done(null, user);
         } else {
             return done(null, false);
+            // or you could create a new account
         }
     });
-});
+}));
