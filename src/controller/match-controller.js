06bac43326cbf45710ebae5444fbe33578ad8a88
DAMoreira <sk4rfac3@gmail.com>
2020-02-16 17:11:04 -0300

    jhjhj

--- src/controller/match-controller.js
+++ src/controller/match-controller.js
@@ -60,7 +60,7 @@ exports.getMatches = (req, res)=>{
 
    exports.getAllMatches = (req, res)=>{
 
-    Match.find({ emisor: req.user.id, receptor: req.user.id}).populate("mascotaEmi").exec( (err, match)=>{ //agregar el receptor
+    Match.find({ emisor: req.user.id, receptor: req.user.id}).exec( (err, match)=>{ //agregar el receptor
          if(err){
              return res.status(400).send({ 'msg': err });
          }
